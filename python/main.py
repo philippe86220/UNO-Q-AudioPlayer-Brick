cd3c429afd6746401d545969e3f2122c4556b04d
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-05-08 10:35:50 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -18,7 +18,9 @@ for attempt in range(30):
 
 player.set_volume(50)
 
-result = player.play("http://icecast.radiofrance.fr/franceinfo-lofi.mp3")
+result = player.play("/hosthome/test.mp3")
+# result = player.play("http://icecast.radiofrance.fr/franceinfo-lofi.mp3")
+
 print(result)
 
 App.run()
